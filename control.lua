699a1ec3893c0f665bb8487047e84bd8b505c425
k390983 <kaikitagawajones@gmail.com>
2020-02-06 07:33:03 +0900

    basic mouse input

--- control.lua
+++ control.lua
@@ -12,3 +12,41 @@ function createBall(x, y, vx, vy, r, m, e)
 	table.insert(world.objects, ball)
 
 end
+
+function mouseInput(dt)
+	if love.mouse.isDown(1) then
+		local x = love.mouse.getX()
+		local y = love.mouse.getY()
+
+		for nameA, A in pairs(world.objects) do
+			if isPointInCircle(x, y, A) then
+				A.vx = (x - A.x) / dt
+				A.vy = (y - A.y) / dt
+				A.x = x
+				A.y = y
+
+			else
+				if not mouseState then
+					createBall(
+						x,
+						y,
+						0,
+						0,
+						math.random(10, 30),
+						math.random(50, 200),
+						1
+					)
+					mouseState = true
+
+				end
+
+			end
+
+		end
+
+	else
+		mouseState = false
+
+	end
+
+end
--- maths.lua
+++ maths.lua
@@ -7,10 +7,18 @@ function isColliding(a, b)
 
 end
 
-function getDistance(a, b)
-	return (a.x - b.x) ^ 2 + (a.y - b.y) ^ 2
+function isPointInCircle(x, y, a)
+	local d2 = (a.x - x) ^ 2 + (a.y - y) ^ 2
+	
+	if d2 < a.r ^ 2  then
+		return true	
+	else
+		return false
+	end
 
 end
+
+function getDistance(a, b) return (a.x - b.x) ^ 2 + (a.y - b.y) ^ 2 end
 
 function getCollisionDirection(a, b)
 	
